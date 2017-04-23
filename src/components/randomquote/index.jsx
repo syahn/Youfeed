ed32727bd185f29a03153b370fc92ff2ed6b790c
Frank Ahn <frankahn9@gmail.com>
2017-04-23 15:00:12 +0900

    Change Random quote design url to JSONP

--- src/components/randomquote/index.jsx
+++ src/components/randomquote/index.jsx
@@ -41,7 +41,7 @@ class RandomQuote extends Component {
   componentWillMount() {
     const { mode } = this.props;
     const urlProgramming = 'https://quotes.stormconsultancy.co.uk/quotes/random.json&callback=?';
-    const urlDesign = 'https://quotesondesign.com/wp-json/posts';
+    const urlDesign = 'https://quotesondesign.com/wp-json/posts&callback=?';
 
     let url = (mode === 'Programming') ? urlProgramming : urlDesign;
     this.getJson(url).then(e => {
